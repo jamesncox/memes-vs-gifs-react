ee381e333b4c29d8a525d32a97304ef2dedf31bd
jamesncox <jamesncox@yahoo.com>
2020-02-10 20:57:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from 'react-router';
+import "./styles.css";
+import NavBar from './components/NavBar'
+import GifGameCard from './components/GifGameCard'
+import MemeGameCard from './components/MemeGameCard'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <h1>Memes Vs. Gifs</h1>
+      <MemeGameCard />
+      <GifGameCard />
     </div>
   );
 }
-
-export default App;
