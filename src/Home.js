726504b0a7e34f0e92759d6356717006344e0355
jamesncox <jamesncox@yahoo.com>
2020-02-11 07:47:14 -0600

    Add Home.js

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from 'react-router';
+import "./stylesheets/home.css";
+import NavBar from './components/NavBar'
+import GifGameCard from './components/GifGameCard'
+import MemeGameCard from './components/MemeGameCard'
+
+export default function App() {
+    return (
+        <div className="App">
+            <NavBar />
+            <h1>Memes Vs. Gifs</h1>
+            <MemeGameCard />
+            <GifGameCard />
+        </div>
+    );
+}
