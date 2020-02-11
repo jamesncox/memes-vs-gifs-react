68ebb1272ab56bb3cbaf8c42ab6bc1d882b961a5
jamesncox <jamesncox@yahoo.com>
2020-02-11 09:29:03 -0600

    Add commented-out navbar router link info

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,3 +22,53 @@ export default function NavBar(props) {
         </div>
     )
 }
+
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+// export default function NavBar(props) {
+
+
+//   return (
+//     <Router>
+//       <div class="navbar">
+//         <div class="dropdown">
+//           <button class="dropbtn">User
+//             <i class="fa fa-caret-down"></i>
+//           </button>
+//           <div class="dropdown-content">
+//             <Link to="/">Sign Up</Link>
+//             <Link to="/">Log In</Link>
+//             <Link to="/">Profile</Link>
+//           </div>
+//         </div>
+//       <a href="#meme">Saved Memes</a>
+//       <a href="#gif">Saved Gifs</a>
+//       <Link to="/">Home</Link>
+
+//       <Switch>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+//           <Route path="/signup">
+//             <Signup />
+//           </Route>
+//           <Route path="/login">
+//             <Login />
+//           </Route>
+//           <Route path="/memes">
+//             <SavedMemes />
+//           </Route>
+//           <Route path="/gifs">
+//             <SavedGifs />
+//           </Route>
+//         </Switch>
+
+//     </div>
+//   </Router>
+//   )
+// }
