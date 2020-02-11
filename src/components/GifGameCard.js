83cbd05ee392a6da592a68c2800e3dd0e9c2c8c9
jamesncox <jamesncox@yahoo.com>
2020-02-10 20:51:00 -0600

    Create GifGameCard.js

---
+++ src/components/GifGameCard.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { GameLinkCard } from "./Styles"
+
+export default function GifGameCard(props) {
+
+    return (
+        <GameLinkCard className="zoom">
+            <h1>That's What You Gif!</h1>
+            <img src={"https://media2.giphy.com/media/ne3xrYlWtQFtC/giphy.gif?cid=790b7611b5bae7bb18ab955cf5ef214ebfa9c3061efe6848&rid=giphy.gif"}></img>
+        </GameLinkCard>
+    )
+}
