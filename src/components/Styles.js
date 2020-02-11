7da02289be0c17b3095c51d150967a181cc33730
jamesncox <jamesncox@yahoo.com>
2020-02-10 20:49:59 -0600

    Create styles.js

---
+++ src/components/Styles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+
+const MemeCard = styled.div`
+  background-color: black;
+  /* border: 4px solid black; */
+  border-radius: .25in;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+`
+
+const MemeButton = styled.button`
+  background-color: black;
+  border-radius: .25in;
+  color: white;
+  font-size: 20px;
+  font-family: Helvetica, sans-serif;
+`
+
+const CaptionButton = styled.button`
+  background-color: black;
+  border-radius: .25in;
+  color: white;
+  font-size: 20px;
+  font-family: Helvetica, sans-serif;
+`
+
+const CaptionCard = styled.div`
+  background-color: black;
+  color: white;
+  font-family: Helvetica, sans-serif;
+  font-size: 20px;
+  width: 2.5in;
+  height: 4in;
+  border-radius: .25in;
+  margin: 10px;
+  display: inline-block;
+  padding: 10px;
+  display: inline-table;
+`
+
+const GameLinkCard = styled.div`
+  background-color: rgba(0, 0, 0, 0.75) ;
+  color: white;
+  font-family: Helvetica, sans-serif;
+  font-size: 20px;
+  width: 2.5in;
+  height: 4in;
+  border-radius: .25in;
+  margin: 10px;
+  display: inline-block;
+  padding: 10px;
+  display: inline-table;
+`
+
+
+
+export { MemeCard, MemeButton, CaptionButton, CaptionCard, GameLinkCard }
