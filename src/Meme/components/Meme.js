10e32a0d0e2f7a8a6c1742758b327451176ae85b
jamesncox <jamesncox@yahoo.com>
2020-02-18 21:20:55 -0600

    Update meme/components/Meme.js

---
+++ src/Meme/components/Meme.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import { MemeCard } from "./Styles"
+import { connect } from 'react-redux'
+
+class Meme extends Component {
+
+    render() {
+        return (
+            <>
+                <div className="zoom">
+                    <MemeCard>
+                        <img src={this.props.memesURL} alt={this.props.name} />
+                        <h2>{this.props.chosenCaption}</h2>
+                    </MemeCard>
+                </div>
+            </>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    chosenCaption: state.captions.chosenCaption
+})
+
+export default connect(mapStateToProps)(Meme)
