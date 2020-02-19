b30537c67bbba523a07851e996561138726d23a4
jamesncox <jamesncox@yahoo.com>
2020-02-18 20:49:10 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from 'react-redux'
-import { createStore, compose, applyMiddleware, combineReducers } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import memes from './reducers/memes'
+import rootReducer from './reducers/index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const reducers = combineReducers(
-    {
-        memes
-    }
-)
-
 const store = createStore(
-    reducers,
+    rootReducer,
     composeEnhancers(applyMiddleware(thunk))
 )
 
@@ -26,10 +20,3 @@ ReactDOM.render(
     </Provider>,
     rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-
