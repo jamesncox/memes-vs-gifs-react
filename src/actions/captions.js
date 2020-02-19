f389128900609e83bcd500a24d40e87fb08e2f68
jamesncox <jamesncox@yahoo.com>
2020-02-18 20:39:07 -0600

    Add actions/captions.js

---
+++ src/actions/captions.js
@@ -0,0 +1,20 @@
+import { SET_CAPTIONS } from '../actionTypes'
+
+const setCaptions = captions => {
+    return { type: SET_CAPTIONS, captions: captions }
+}
+
+export const getCaptions = () => {
+    return async dispatch => {
+        try {
+            const res = await fetch("http://localhost:3000/captions")
+            if (!res.ok) {
+                throw res
+            }
+            const captionData = await res.json()
+            dispatch(setCaptions(captionData))
+        } catch (err) {
+            alert("An error occured")
+        }
+    }
+}
