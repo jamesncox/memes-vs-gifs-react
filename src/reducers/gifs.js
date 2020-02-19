3f4af357a395d6b93e891c5332ae9d1ebbc99655
jamesncox <jamesncox@yahoo.com>
2020-02-18 20:46:36 -0600

    Add reducers/gifs.js

---
+++ src/reducers/gifs.js
@@ -0,0 +1,15 @@
+import { SET_QUERIED_GIFS, GEN_RANDOM_GIF, SET_QUERY_INPUT } from '../actionTypes'
+
+export default (state = { all: [], randomGif: null, query: '' }, action) => {
+    switch (action.type) {
+        case SET_QUERY_INPUT:
+            return { ...state, query: action.payload }
+        case SET_QUERIED_GIFS:
+            return { ...state, all: action.gifs }
+        case GEN_RANDOM_GIF:
+            const randGif = state.all[Math.floor(Math.random() * state.all.length)]
+            return { ...state, randomGif: randGif }
+        default:
+            return state
+    }
+}
