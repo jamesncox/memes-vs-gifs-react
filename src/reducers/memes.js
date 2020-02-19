fe69536881e5f267f0940c1fa94689d68967261a
jamesncox <jamesncox@yahoo.com>
2020-02-18 20:44:43 -0600

    Update reducers/memes.js

--- src/reducers/memes.js
+++ src/reducers/memes.js
@@ -1,7 +1,12 @@
-export default (state = [], action) => {
+import { SET_MEMES, GEN_RANDOM_MEME } from '../actionTypes'
+
+export default (state = { all: [], randomMeme: null }, action) => {
     switch (action.type) {
-        case 'SET_MEMES':
-            return action.memes
+        case SET_MEMES:
+            return { ...state, all: action.memes }
+        case GEN_RANDOM_MEME:
+            const randMeme = state.all[Math.floor(Math.random() * state.all.length)]
+            return { ...state, randomMeme: randMeme }
         default:
             return state
     }
