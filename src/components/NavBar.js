f4aa5fcd3792ae2e69f2ea2ee8579dcd8a1914c2
jamesncox <jamesncox@yahoo.com>
2020-02-19 12:49:39 -0600

    Update NavBar with Link to home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Switch,
+    Route,
+    Link,
+    useHistory
+} from 'react-router-dom';
 
 export default function NavBar(props) {
 
@@ -9,66 +17,36 @@ export default function NavBar(props) {
                      <i class="fa fa-caret-down"></i>
                 </button>
                 <div class="dropdown-content">
-                    <a href="#signup">Sign Up</a>
-                    <a href="#login">Login</a>
+                    <Link to="/signup">Sign Up</Link>
+                    <Link to="/login">Login</Link>
                     {/* click user profile to see only user's saved memes and gifs */}
-                    <a href="#profile">Profile</a>
+                    <Link to="/profile">Profile</Link>
                 </div>
             </div>
             {/* click savedmemes to see all users best saved memes */}
-            <a href="#savedmemes">Saved Memes</a>
+            <Link to="/saved-memes">Saved Memes</Link>
             {/* clicked savedgifs to see all users best saved gifs */}
-            <a href="#savedgifs">Saved Gifs</a>
+            <Link to="/saved-gifs">Saved Gifs</Link>
+            <Link to="/">Home</Link>
+
+            {/* <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/signup">
+                    <Signup />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/saved-memes">
+                    <SavedMemes />
+                </Route>
+                <Route path="/saved-gifs">
+                    <SavedGifs />
+                </Route>
+            </Switch> */}
+
         </div>
     )
 }
-
-// import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
-// export default function NavBar(props) {
-
-
-//   return (
-//     <Router>
-//       <div class="navbar">
-//         <div class="dropdown">
-//           <button class="dropbtn">User
-//             <i class="fa fa-caret-down"></i>
-//           </button>
-//           <div class="dropdown-content">
-//             <Link to="/">Sign Up</Link>
-//             <Link to="/">Log In</Link>
-//             <Link to="/">Profile</Link>
-//           </div>
-//         </div>
-//       <a href="#meme">Saved Memes</a>
-//       <a href="#gif">Saved Gifs</a>
-//       <Link to="/">Home</Link>
-
-//       <Switch>
-//           <Route exact path="/">
-//             <Home />
-//           </Route>
-//           <Route path="/signup">
-//             <Signup />
-//           </Route>
-//           <Route path="/login">
-//             <Login />
-//           </Route>
-//           <Route path="/memes">
-//             <SavedMemes />
-//           </Route>
-//           <Route path="/gifs">
-//             <SavedGifs />
-//           </Route>
-//         </Switch>
-
-//     </div>
-//   </Router>
-//   )
-// }
