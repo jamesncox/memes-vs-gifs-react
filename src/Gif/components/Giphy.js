e98d5607e90c4c7e8b6288aa337b0b127f80dd75
jamesncox <jamesncox@yahoo.com>
2020-02-18 21:08:38 -0600

    Update gif/components/Giphy.js

---
+++ src/Gif/components/Giphy.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react"
+import { GiphyCard } from "./Styles"
+import { connect } from 'react-redux'
+
+class Giphy extends Component {
+
+    render() {
+        return (
+            <>
+                <div className="zoom">
+                    <GiphyCard>
+                        <img src={this.props.giphyURL} alt={this.props.name} />
+                        <h2>{this.props.chosenCaption}</h2>
+                    </GiphyCard>
+                </div>
+            </>
+        )
+    }
+}
+const mapStateToProps = state => ({
+    chosenCaption: state.captions.chosenCaption
+})
+
+export default connect(mapStateToProps)(Giphy)
