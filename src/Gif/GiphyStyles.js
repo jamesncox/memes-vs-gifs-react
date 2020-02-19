e645e64a99c59792c25a4124f771d87540012ebe
jamesncox <jamesncox@yahoo.com>
2020-02-18 21:14:59 -0600

    Update gif/GiphyStyles.js

--- src/Gif/GiphyStyles.js
+++ src/Gif/GiphyStyles.js
@@ -1,32 +1,47 @@
 import styled from "styled-components"
 
-const MemeCard = styled.div`
-  background-color: black;
-  /* border: 4px solid black; */
+const GiphyCard = styled.div`
+  background-color: #000308d2;
   border-radius: .25in;
   display: inline-block;
   margin: 10px;
   padding: 10px;
 `
 
-const MemeButton = styled.button`
-  background-color: black;
-  border-radius: .25in;
-  color: white;
+const GiphyButton = styled.button`
+  color: buttontext;
+  border-radius: .10in;
+  color: black;
   font-size: 20px;
   font-family: Helvetica, sans-serif;
 `
 
 const CaptionButton = styled.button`
-  background-color: black;
-  border-radius: .25in;
+   color: buttontext;
+  border-radius: .10in;
+  color: black;
+  font-size: 20px;
+  font-family: Helvetica, sans-serif;
+`
+
+const SelectButton = styled.button`
+  background-color: #000308d2;
+  border-radius: .10in;
   color: white;
+  font-size: 20px;
+  font-family: Helvetica, sans-serif;
+`
+
+const NextBtn = styled.button`
+  color: buttontext;
+  border-radius: .10in;
+  color: black;
   font-size: 20px;
   font-family: Helvetica, sans-serif;
 `
 
 const CaptionCard = styled.div`
-  background-color: black;
+  background-color: #32003bd2;
   color: white;
   font-family: Helvetica, sans-serif;
   font-size: 20px;
@@ -39,20 +54,73 @@ const CaptionCard = styled.div`
   display: inline-table;
 `
 
-const GameLinkCard = styled.div`
-  background-color: rgba(0, 0, 0, 0.75) ;
-  color: white;
-  font-family: Helvetica, sans-serif;
+const MyForm = styled.form`
+  color: buttontext;
+  border-radius: .10in;
+  color: black;
   font-size: 20px;
-  width: 2.5in;
-  height: 4in;
-  border-radius: .25in;
-  margin: 10px;
-  display: inline-block;
-  padding: 10px;
-  display: inline-table;
+  font-family: Helvetica, sans-serif;
 `
 
+export { GiphyCard, GiphyButton, CaptionButton, CaptionCard, NextBtn, MyForm, SelectButton }
 
 
-export { MemeCard, MemeButton, CaptionButton, CaptionCard, GameLinkCard }
+
+// import styled from "styled-components"
+
+// const MemeCard = styled.div`
+//   background-color: black;
+//   /* border: 4px solid black; */
+//   border-radius: .25in;
+//   display: inline-block;
+//   margin: 10px;
+//   padding: 10px;
+// `
+
+// const MemeButton = styled.button`
+//   background-color: black;
+//   border-radius: .25in;
+//   color: white;
+//   font-size: 20px;
+//   font-family: Helvetica, sans-serif;
+// `
+
+// const CaptionButton = styled.button`
+//   background-color: black;
+//   border-radius: .25in;
+//   color: white;
+//   font-size: 20px;
+//   font-family: Helvetica, sans-serif;
+// `
+
+// const CaptionCard = styled.div`
+//   background-color: black;
+//   color: white;
+//   font-family: Helvetica, sans-serif;
+//   font-size: 20px;
+//   width: 2.5in;
+//   height: 4in;
+//   border-radius: .25in;
+//   margin: 10px;
+//   display: inline-block;
+//   padding: 10px;
+//   display: inline-table;
+// `
+
+// const GameLinkCard = styled.div`
+//   background-color: rgba(0, 0, 0, 0.75) ;
+//   color: white;
+//   font-family: Helvetica, sans-serif;
+//   font-size: 20px;
+//   width: 2.5in;
+//   height: 4in;
+//   border-radius: .25in;
+//   margin: 10px;
+//   display: inline-block;
+//   padding: 10px;
+//   display: inline-table;
+// `
+
+
+
+// export { MemeCard, MemeButton, CaptionButton, CaptionCard, GameLinkCard }
