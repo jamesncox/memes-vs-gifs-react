70ec3cf2ae2b1a0a8955424268f7aca5cfb7a856
jamesncox <jamesncox@yahoo.com>
2020-02-18 21:25:32 -0600

    Update meme/MemeStyles.js

--- src/Meme/MemeStyles.js
+++ src/Meme/MemeStyles.js
@@ -2,7 +2,6 @@ import styled from "styled-components"
 
 const MemeCard = styled.div`
   background-color: black;
-  /* border: 4px solid black; */
   border-radius: .25in;
   display: inline-block;
   margin: 10px;
@@ -15,28 +14,6 @@ const MemeButton = styled.button`
   color: white;
   font-size: 20px;
   font-family: Helvetica, sans-serif;
-`
-
-const CaptionButton = styled.button`
-  background-color: black;
-  border-radius: .25in;
-  color: white;
-  font-size: 20px;
-  font-family: Helvetica, sans-serif;
-`
-
-const CaptionCard = styled.div`
-  background-color: black;
-  color: white;
-  font-family: Helvetica, sans-serif;
-  font-size: 20px;
-  width: 2.5in;
-  height: 4in;
-  border-radius: .25in;
-  margin: 10px;
-  display: inline-block;
-  padding: 10px;
-  display: inline-table;
 `
 
 const GameLinkCard = styled.div`
@@ -55,4 +32,4 @@ const GameLinkCard = styled.div`
 
 
 
-export { MemeCard, MemeButton, CaptionButton, CaptionCard, GameLinkCard }
+export { MemeCard, MemeButton, GameLinkCard }
