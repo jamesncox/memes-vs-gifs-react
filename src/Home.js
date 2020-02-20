62ca5ee93b2cb782e684c0ab5517beb55f9b874d
jamesncox <jamesncox@yahoo.com>
2020-02-20 11:50:30 -0600

    Refactor home.js into a class component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Redirect,
@@ -7,31 +7,40 @@ import {
     Link,
     useHistory
 } from 'react-router-dom';
-// import './stylesheets/home.css';
 import GifGameCard from './components/GifGameCard'
 import MemeGameCard from './components/MemeGameCard'
+import { connect } from 'react-redux'
+import { CLEAR_MEME } from './actionTypes'
 
-export default function Home() {
+class Home extends Component {
 
-    let history = useHistory()
+    // let history = useHistory()
 
-    const redirectToMemeGame = () => {
-        history.push('/meme-game')
-        // window.location.reload(false)
+    // const redirectToMemeGame = () => {
+    //     history.push('/meme-game')
+    //     // window.location.reload(false)
+    // }
+
+    // const redirectToGifGame = () => {
+    //     history.push('/gif-game')
+    //     // window.location.reload(false)
+    // }
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <h1 className="header">Memes Vs. Gifs</h1>
+                    <MemeGameCard handleNav={redirectToMemeGame} />
+                    <GifGameCard handleNav={redirectToGifGame} />
+                </div>
+            </Router>
+        );
     }
-
-    const redirectToGifGame = () => {
-        history.push('/gif-game')
-        // window.location.reload(false)
-    }
-
-    return (
-        <Router>
-            <div>
-                <h1 className="header">Memes Vs. Gifs</h1>
-                <MemeGameCard handleNav={redirectToMemeGame} />
-                <GifGameCard handleNav={redirectToGifGame} />
-            </div>
-        </Router>
-    );
 }
+
+const mapDispatchToProps = dispatch => ({
+    clearMeme: () => dispatch({ type: CLEAR_MEME })
+})
+
+export default connect(null, mapDispatchToProps)(Home)
