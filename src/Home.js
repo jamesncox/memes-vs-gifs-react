dc1a75f472d1ad41ef917558fef0402a0a5b3b47
jamesncox <jamesncox@yahoo.com>
2020-02-20 16:09:04 -0600

    Comment out class component in Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,79 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
     BrowserRouter as Router,
-    Redirect,
-    Switch,
-    Route,
-    Link,
     useHistory
 } from 'react-router-dom';
 import GifGameCard from './components/GifGameCard'
 import MemeGameCard from './components/MemeGameCard'
-import { connect } from 'react-redux'
-import { CLEAR_MEME } from './actionTypes'
 
-class Home extends Component {
+export default function Home(props) {
 
-    // let history = useHistory()
+    let history = useHistory()
 
-    // const redirectToMemeGame = () => {
-    //     history.push('/meme-game')
-    //     // window.location.reload(false)
-    // }
-
-    // const redirectToGifGame = () => {
-    //     history.push('/gif-game')
-    //     // window.location.reload(false)
-    // }
-
-    render() {
-        return (
-            <Router>
-                <div>
-                    <h1 className="header">Memes Vs. Gifs</h1>
-                    <MemeGameCard handleNav={redirectToMemeGame} />
-                    <GifGameCard handleNav={redirectToGifGame} />
-                </div>
-            </Router>
-        );
+    const redirectToMemeGame = () => {
+        history.push('/meme-game')
+        // window.location.reload(false)
     }
+
+    const redirectToGifGame = () => {
+        history.push('/gif-game')
+        // window.location.reload(false)
+    }
+
+    return (
+        <Router>
+            <div>
+                <h1 className="header">Memes Vs. Gifs</h1>
+                <MemeGameCard handleNav={redirectToMemeGame} />
+                <GifGameCard handleNav={redirectToGifGame} />
+            </div>
+        </Router>
+    );
 }
 
-const mapDispatchToProps = dispatch => ({
-    clearMeme: () => dispatch({ type: CLEAR_MEME })
-})
+// import React, { Component } from 'react';
+// import {
+//     BrowserRouter as Router,
+//     Redirect,
+//     Switch,
+//     Route,
+//     Link,
+//     useHistory
+// } from 'react-router-dom';
+// import GifGameCard from './components/GifGameCard'
+// import MemeGameCard from './components/MemeGameCard'
+// import { connect } from 'react-redux'
+// import { CLEAR_MEME } from './actionTypes'
 
-export default connect(null, mapDispatchToProps)(Home)
+// class Home extends Component {
+
+//     // let history = useHistory()
+
+//     // const redirectToMemeGame = () => {
+//     //     history.push('/meme-game')
+//     //     // window.location.reload(false)
+//     // }
+
+//     // const redirectToGifGame = () => {
+//     //     history.push('/gif-game')
+//     //     // window.location.reload(false)
+//     // }
+
+//     render() {
+//         return (
+//             <Router>
+//                 <div>
+//                     <h1 className="header">Memes Vs. Gifs</h1>
+//                     <MemeGameCard handleNav={redirectToMemeGame} />
+//                     <GifGameCard handleNav={redirectToGifGame} />
+//                 </div>
+//             </Router>
+//         );
+//     }
+// }
+
+// const mapDispatchToProps = dispatch => ({
+//     clearMeme: () => dispatch({ type: CLEAR_MEME })
+// })
+
+// export default connect(null, mapDispatchToProps)(Home)
