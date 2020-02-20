3f57cc02098c8337326c21d80368271e13b713d2
jamesncox <jamesncox@yahoo.com>
2020-02-20 09:48:08 -0600

    Add window.reload to home.js redirects

--- src/Home.js
+++ src/Home.js
@@ -13,14 +13,20 @@ import MemeGameCard from './components/MemeGameCard'
 
 export default function Home() {
 
+    // is there a way to clear the data with useHistory? Like refresh page does??
+    // window.location.reload(false) causes the fetch error to fire and it's not very clean
+    // should try writing a reducer that resets state and dispatched here in the redirect functions
+
     let history = useHistory()
 
     const redirectToMemeGame = () => {
         history.push('/meme-game')
+        window.location.reload(false)
     }
 
     const redirectToGifGame = () => {
         history.push('/gif-game')
+        window.location.reload(false)
     }
 
     return (
