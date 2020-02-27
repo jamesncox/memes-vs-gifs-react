0c3be5d73ecd58451a2f88df9a14dd02cf9d9de1
jamesncox <jamesncox@yahoo.com>
2020-02-27 08:07:19 -0600

    Add loading message/gif to meme and gif game cards

--- src/components/GifGameCard.js
+++ src/components/GifGameCard.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { GameLinkCard } from './Styles'
+import { connect } from 'react-redux'
 
-export default function GifGameCard(props) {
+class GifGameCard extends Component {
 
-    return (
-        <GameLinkCard onClick={props.handleNav} className="zoom">
-            <h1>That's What You Gif!</h1>
-            <img src={"https://media2.giphy.com/media/ne3xrYlWtQFtC/giphy.gif?cid=790b7611b5bae7bb18ab955cf5ef214ebfa9c3061efe6848&rid=giphy.gif"} alt={"Calculating"}></img>
-        </GameLinkCard>
-    )
+    render() {
+        if (this.props.loading) {
+            return (
+                <GameLinkCard>
+                    <h3>Loading...</h3>
+                    <img src={"https://i.giphy.com/media/3ov9k0BZQL358k458s/giphy.webp"} alt={"Loading..."} />
+                </GameLinkCard>
+            )
+        } else {
+            return (
+                <GameLinkCard onClick={this.props.handleNav} className="zoom">
+                    <h1>That's What You Gif!</h1>
+                    <img src={"https://media2.giphy.com/media/ne3xrYlWtQFtC/giphy.gif?cid=790b7611b5bae7bb18ab955cf5ef214ebfa9c3061efe6848&rid=giphy.gif"} alt={"Calculating"}></img>
+                </GameLinkCard>
+            )
+        }
+    }
 }
+
+const mapStateToProps = state => ({
+    loading: state.gifs.loading
+})
+
+export default connect(mapStateToProps)(GifGameCard)
--- src/components/MemeGameCard.js
+++ src/components/MemeGameCard.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { GameLinkCard } from './Styles'
+import { connect } from 'react-redux'
 
-export default function MemeGameCard(props) {
+class MemeGameCard extends Component {
 
-    return (
-        <GameLinkCard onClick={props.handleNav} className="zoom">
-            <h1>What Do You Meme?</h1>
-            <img src={"https://i.kym-cdn.com/photos/images/newsfeed/000/000/130/disaster-girl.jpg"} alt={"Girl Burning House"}></img>
-        </GameLinkCard>
-    )
+    render() {
+        if (this.props.loading) {
+            return (
+                <GameLinkCard>
+                    <h3>Loading...</h3>
+                    <img src={"https://i.giphy.com/media/3ov9k0BZQL358k458s/giphy.webp"} alt={"Loading..."} />
+                </GameLinkCard>
+            )
+        } else {
+            return (
+                <GameLinkCard onClick={this.props.handleNav} className="zoom">
+                    <h1>What Do You Meme?</h1>
+                    <img src={"https://i.kym-cdn.com/photos/images/newsfeed/000/000/130/disaster-girl.jpg"} alt={"Girl Burning House"}></img>
+                </GameLinkCard>
+            )
+        }
+    }
 }
+
+const mapStateToProps = state => ({
+    loading: state.gifs.loading
+})
+
+export default connect(mapStateToProps)(MemeGameCard)
