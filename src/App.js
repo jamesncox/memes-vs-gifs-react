b49d4702e023497b077f315ffef178bf1e1208e9
jamesncox <jamesncox@yahoo.com>
2020-02-28 16:37:57 -0600

    Refactor getCookie into actions/sessions.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Signup from './User/Signup'
 import { connect } from 'react-redux';
 import { getMemes, getSavedMemes } from './actions/memes'
 import { getSavedGifs } from './actions/gifs'
-import { setToken } from './actions/sessions'
+import { getToken } from './actions/sessions'
 
 class App extends Component {
 
@@ -25,10 +25,7 @@ class App extends Component {
     this.props.getMemes()
     this.props.getSavedMemes()
     this.props.getSavedGifs()
-    // need to put fetch for cookies here to set cookie to always have available
-    fetch('http://localhost:3000/api/v1/auth-check')
-      .then(res => res.json())
-      .then(token => this.props.setToken(token.csrf_auth_token))
+    this.props.getToken()
   }
 
   render() {
@@ -69,4 +66,4 @@ class App extends Component {
   }
 }
 
-export default connect(null, { getMemes, getSavedMemes, getSavedGifs, setToken })(App)
+export default connect(null, { getMemes, getSavedMemes, getSavedGifs, getToken })(App)
--- src/actions/sessions.js
+++ src/actions/sessions.js
@@ -1,7 +1,16 @@
 import { SET_TOKEN } from '../actionTypes'
 
-export const setToken = (token) => {
+const setToken = (token) => {
     return dispatch => {
+        console.log(token)
         dispatch({ type: SET_TOKEN, payload: token })
+    }
+}
+
+export function getToken() {
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/auth-check')
+            .then(res => res.json())
+            .then(token => dispatch(setToken(token.csrf_auth_token)))
     }
 }
