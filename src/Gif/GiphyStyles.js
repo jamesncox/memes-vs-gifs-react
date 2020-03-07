5c4d2ea46457e7aa69bac23ecb9df2aab523c1a7
jamesncox <jamesncox@yahoo.com>
2020-03-06 23:43:34 -0600

    Add message to GiphyHome if user not logged in

--- src/Gif/GiphyStyles.js
+++ src/Gif/GiphyStyles.js
@@ -31,66 +31,27 @@ const SaveButton = styled.button`
   font-size: 20px;
   font-family: Helvetica, sans-serif;
 `
+const LoginCard = styled.div`
+  background-color: rgba(0, 0, 0, 0.90) ;
+  color: white;
+  font-family: Helvetica, sans-serif;
+  font-size: 20px;
+  width: 1in;
+  height: 1in;
+  border-radius: .25in;
+  margin: 10px;
+  display: inline-block;
+  padding: 10px;
+  display: inline-table;
+`
 
-export { GiphyCard, NextBtn, MyForm, SaveButton }
-
-
-
-// import styled from "styled-components"
-
-// const MemeCard = styled.div`
-//   background-color: black;
-//   /* border: 4px solid black; */
-//   border-radius: .25in;
-//   display: inline-block;
-//   margin: 10px;
-//   padding: 10px;
-// `
-
-// const MemeButton = styled.button`
-//   background-color: black;
-//   border-radius: .25in;
-//   color: white;
-//   font-size: 20px;
-//   font-family: Helvetica, sans-serif;
-// `
-
-// const CaptionButton = styled.button`
-//   background-color: black;
-//   border-radius: .25in;
-//   color: white;
-//   font-size: 20px;
-//   font-family: Helvetica, sans-serif;
-// `
-
-// const CaptionCard = styled.div`
-//   background-color: black;
-//   color: white;
-//   font-family: Helvetica, sans-serif;
-//   font-size: 20px;
-//   width: 2.5in;
-//   height: 4in;
-//   border-radius: .25in;
-//   margin: 10px;
-//   display: inline-block;
-//   padding: 10px;
-//   display: inline-table;
-// `
-
-// const GameLinkCard = styled.div`
-//   background-color: rgba(0, 0, 0, 0.75) ;
-//   color: white;
-//   font-family: Helvetica, sans-serif;
-//   font-size: 20px;
-//   width: 2.5in;
-//   height: 4in;
-//   border-radius: .25in;
-//   margin: 10px;
-//   display: inline-block;
-//   padding: 10px;
-//   display: inline-table;
-// `
+const CloseButton = styled.button`
+  border-radius: .10in;
+  background-color: rgba(0, 0, 0, 0.00);
+  color:  #F8F8FF;
+  font-size: 20px;
+  font-family: Helvetica, sans-serif;
+`
 
 
-
-// export { MemeCard, MemeButton, CaptionButton, CaptionCard, GameLinkCard }
+export { GiphyCard, NextBtn, MyForm, SaveButton, LoginCard, CloseButton }
--- src/Gif/components/GiphyHome.js
+++ src/Gif/components/GiphyHome.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
 import GiphyContainer from './GiphyContainer'
 import CaptionContainer from '../../Caption/CaptionContainer'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { LoginCard, CloseButton } from '../GiphyStyles'
 
 class GiphyHome extends Component {
 
-    render() {
+    state = {
+        shouldRedirect: false
+    }
+
+    handleClick = () => {
+        this.setState({
+            shouldRedirect: true
+        })
+    }
+
+    renderLoginMessage = () => {
         return (
-            <div>
-                <h1 className="header">That's What You Gif!</h1>
-                <GiphyContainer />
-                <CaptionContainer />
-                <h4 style={{ width: "98%", backgroundColor: "rgba(0, 0, 0, 0.50)" }}>Memes Vs Gifs</h4>
-            </div>
+            <LoginCard>
+                <img src={"https://media.giphy.com/media/eeL8EcBBTwSMLACw6F/giphy.gif"} alt={"Elf congratulations"} />
+                <h3> </h3>
+                You must be logged in to create a gif!
+                    <h3> </h3>
+                <CloseButton onClick={this.handleClick}>Login</CloseButton>
+            </LoginCard>
         )
+    }
+
+    render() {
+        if (this.props.loggedIn === false) {
+            return this.state.shouldRedirect ? (<Redirect to="/profile" />) : (
+                <>
+                    <h1 className="header">That's What You Gif!</h1>
+                    {this.renderLoginMessage()}
+                </>
+            )
+        } else {
+            return (
+                <div>
+                    <h1 className="header">That's What You Gif!</h1>
+                    <GiphyContainer />
+                    <CaptionContainer />
+                    <h4 style={{ width: "98%", backgroundColor: "rgba(0, 0, 0, 0.50)" }}>Memes Vs Gifs</h4>
+                </div>
+            )
+        }
     }
 }
 
-export default GiphyHome
+const mapStateToProps = state => ({
+    loggedIn: state.users.loggedIn
+})
+
+export default connect(mapStateToProps)(GiphyHome)
