72016f5987b421072b60447959b95affb6958819
jamesncox <jamesncox@yahoo.com>
2020-03-13 11:18:38 -0500

    Remove link from giphy attribute mark

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,9 +29,11 @@ class NavBar extends Component {
                 </div>
                 <Link to="/saved-memes">Memes</Link>
                 <Link to="/saved-gifs">Gifs</Link>
-                <a href="https://www.giphy.com" target="_blank" style={{ float: "right", marginTop: "0px" }} >
-                    <img src={require('../assets/images/PoweredBy_200px-Black_HorizLogo.png')} alt="Powered By Giphy" />
-                </a>
+                <img
+                    src={require('../assets/images/PoweredBy_200px-Black_HorizLogo.png')}
+                    alt="Powered By Giphy"
+                    style={{ float: "right", marginTop: "18px" }}
+                />
             </div >
         )
     }
