b0f7c8336d5e9f4beced5787c686b1ecd028c49b
jamesncox <jamesncox@yahoo.com>
2020-03-19 08:53:02 -0500

    Update About component

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ class About extends Component {
                 <AboutCard>
                     <p>Memes vs Gifs is created by James Cox, a recent graduate of Flatiron School.</p>
                     <GuidelineCard>
-                        <p>Please create all the gifs and memes your heart desires!</p>
+                        <p>Please create all the gifs and memes your heart desires.</p>
                         <p>You can upload images from the web, and create your own captions. Which is totally awesome. But there are some guidelines:</p>
                         <ul>
                             <p>Images must be suitable for work / office environments. (No nudity, violent images, propaganda, etc.)</p>
@@ -17,7 +17,7 @@ class About extends Component {
                             <p></p>
                         </ul>
                         <p>If any images or captions violate these terms, they will be removed and the user associated with them will be deleted, with all their saved gifs and memes.</p>
-                        <p>Enjoy the hell out of this app! But use it responsibly!</p>
+                        <p>Enjoy the hell out of this app. But use it responsibly.</p>
                         <p>Ability to share your creations on social media coming soon...</p>
                     </GuidelineCard>
                     <p>I welcome feedback, ideas, criticism and reports of bugs or issues: jamesnathanielcox@gmail.com </p>
